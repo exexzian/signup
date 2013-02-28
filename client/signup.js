36d602b104fb2b5adafc5ebcf7afb4bd764966a3
schofld <d.schofld@gmail.com>
2013-02-28 17:35:48 +0100

    ok so the login works now

--- client/signup.js
+++ client/signup.js
@@ -1,12 +1,20 @@
 Template.user_loggedout.events({
 	"click #login": function (e, tmpl) {
 		console.log("clicked login");
-		Meteor.loginWithPassword({username: "schofld", email: "d.schofld@gmail.com", password: "testpassword"}, function (err) {
-				console.log(err);
-			}
-		)
+	    var username = "schofld";
+	    var email = "d.schofld@gmail.com";
+    	var password = "testpassword";
+		Meteor.loginWithPassword(username, password);
 	}
 });
+
+Template.user_loggedout.events({
+	"click #signup": function (e, tmpl) {
+		console.log("clicked signup");
+		Accounts.createUser({username: "schofld", email: "d.schofld@gmail.com", password: "testpassword"});
+	}
+});
+
 Template.user_loggedin.events({
 	"click #logout": function (e, tmpl) {
 		Meteor.logout()
